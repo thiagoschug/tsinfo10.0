fbd3335bc88b5da3fc858908e238ea1133649dfe
thiagoschug <>
2024-06-17 13:07:44 -0300

    PrimeiroCMT

--- ts-info/src/App.js
+++ ts-info/src/App.js
@@ -1,20 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import PaginaInicio from './components/PaginaInicio';
 import Pagina1 from './components/Pagina1';
 import Pagina2 from './components/Pagina2';
 import Pagina3 from './components/Pagina3';
-
+import Progressbar from './components/Progressbar'
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <PaginaInicio/>
-      <Pagina1 />
-      <Pagina2 />
-      <Pagina3 />
-    </div>
+    <Router>
+      <div className="App">
+     
+        <Header />
+        <PaginaInicio />
+        <Pagina1 />
+        <Pagina2 />
+        <Pagina3 />
+      </div>
+    </Router>
   );
 }
 
--- ts-info/src/components/Header.js
+++ ts-info/src/components/Header.js
@@ -2,21 +2,21 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
+import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
 import Logo from './TS.svg'; // Importe o logotipo
 import styles from './header.module.css'; // Importar o arquivo de módulo CSS
 
 
 const pages = ['Home', 'Serviços', 'Sobre'];
-const settings = ['Perfil', 'Account', 'Dashboard', 'Logout'];
+const settings = ['Perfil', 'Conta', 'Sair'];
 
 function Header() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -56,50 +56,18 @@ function Header() {
 
   return (
     <AppBar position="fixed" className={`${styles.header} ${isScrolled ? styles.scrolled : ''} ${styles.transparent}`} style={{ boxShadow: 'none' , marginTop: '30px' }}>
-    <Container maxWidth="xl">
-      <Toolbar disableGutters>
-        <Typography variant="h6" noWrap component="a" href="#app-bar-with-responsive-menu">
-          <img src={Logo} alt="Logo" style={{ marginRight: '16px', height: '70px' }} />
-        </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-              keepMounted
-              transformOrigin={{ vertical: 'top', horizontal: 'left' }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: 'block', md: 'none' } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{ mr: 2, display: { xs: 'flex', md: 'none' }, flexGrow: 1, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', color: 'inherit', textDecoration: 'none' }}
-          >
-            TS 
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography variant="h6" noWrap component="a" href="#app-bar-with-responsive-menu">
+            <img src={Logo} alt="Logo" style={{ marginRight: '16px', height: '100px' }} /> {/* Ajuste o tamanho do logo */}
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: 'center' } }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexGrow: 1 }}> {/* Centralize os botões */}
             {pages.map((page) => (
               <Button
                 key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 1, mx: 5, color: 'white', display: 'block', marginRight: '30px' }} // Ajuste aqui
+                href={`#${page.toLowerCase()}`} // Adicione o href para rolar para a seção correta na página
+                sx={{ mx: 2, color: 'white' }} // Ajuste o espaçamento horizontal
               >
                 {page}
               </Button>
